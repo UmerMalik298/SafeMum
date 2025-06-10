87a08117c0656a630910a8bf2741df763aa42169
UmerFarooq <umer23672@gmail.com>
2025-06-10 14:23:41 +0500

    Forgot and Reset Password

---
+++ SafeMum.Application/Features/Users/ForgotPassword/ForgotPasswordHandler.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+using SafeMum.Application.Interfaces;
+
+namespace SafeMum.Application.Features.Users.ForgotPassword
+{
+    public class ForgotPasswordHandler : IRequestHandler<ForgotPasswordRequest, ForgotPasswordResponse>
+    {
+        private readonly Supabase.Client _client;
+        public ForgotPasswordHandler(ISupabaseClientFactory clientFactory)
+        {
+            _client = clientFactory.GetClient();
+            
+        }
+        public async Task<ForgotPasswordResponse> Handle(ForgotPasswordRequest request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                await _client.Auth.ResetPasswordForEmail(request.Email);
+
+                return new ForgotPasswordResponse
+                {
+                    Success = true,
+                    Message = "Password Reset Email has been sent"
+                };
+            }
+            catch (Exception ex)
+            {
+
+                return new ForgotPasswordResponse
+                {
+                    Success = false,
+                    Message = "Faild to send reset Email"+ ex.ToString()
+                };
+            }
+          
+        }
+    }
+}
