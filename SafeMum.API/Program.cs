c7bb102dc5968b0dd279ef1c19fd8aec25f7c61f
UmerFarooq <umer23672@gmail.com>
2025-06-10 11:01:10 +0500

    JWT Token and Refresh Token

--- SafeMum.API/Program.cs
+++ SafeMum.API/Program.cs
@@ -1,5 +1,6 @@
 
 
+using Microsoft.IdentityModel.Tokens;
 using SafeMum.API.EndPoints;
 using SafeMum.Infrastructure.Configuration;
 
@@ -10,6 +11,28 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddInfrastructure();
 builder.Services.AddMediatR(cfg =>
     cfg.RegisterServicesFromAssembly(typeof(Program).Assembly));
+var jwtKeys = builder.Configuration.GetSection("JwtSettings");
+
+string authority = jwtKeys["Authority"];
+string issuer = jwtKeys["Issuer"];
+builder.Services.AddAuthentication("Bearer")
+    .AddJwtBearer("Bearer", options =>
+    {
+        options.Authority = authority;
+        
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidIssuer = issuer,
+            ValidateAudience = false,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true
+        };
+    });
+
+builder.Services.AddAuthentication();
+builder.Services.AddAuthorization();
+
 
 var app = builder.Build();
 
--- SafeMum.Application/Features/Users/Login/LoginUserRequest.cs
+++ SafeMum.Application/Features/Users/Login/LoginUserRequest.cs
@@ -22,12 +22,16 @@ namespace SafeMum.Application.Features.Users.Login
 
     public class LoginUserResponse
     {
+        public string UserId { get; set; }
         public bool Success { get; set; }
         public string Token { get; set; }
         public DateTime ExpiresAt { get; set; }
+
+        public string RefreshToken { get; set; }
+
+
         public string Email { get; set; }
-        public string Username { get; set; }
-        public string Role { get; set; }
+ 
         public string Message { get; set; }
         public List<string> Errors { get; set; } = new();
     }
