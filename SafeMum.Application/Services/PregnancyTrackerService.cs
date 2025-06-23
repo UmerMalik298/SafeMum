1487b87d5c7813c0e3cea5ff1f4555e48478573a
UmerFarooq <umer23672@gmail.com>
2025-06-23 21:34:10 +0500

    services

---
+++ SafeMum.Application/Services/PregnancyTrackerService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SafeMum.Application.Interfaces;
+
+namespace SafeMum.Infrastructure.Services
+{
+    public class PregnancyTrackerService : IPregnancyTrackerService
+    {
+        public int CalculateWeekFromEDD(DateTime edd)
+        {
+            var weeksRemaining = (edd - DateTime.UtcNow.Date).TotalDays / 7;
+            var currentWeek = 40 - (int)weeksRemaining;
+            return Math.Clamp(currentWeek, 1, 40);
+        }
+
+
+
+        public int CalculateWeekFromLMP(DateTime lmp)
+        {
+            if (lmp > DateTime.UtcNow.Date)
+            {
+                throw new ArgumentException("LMP cannot be in the future");
+            }
+
+            var totalDays = (DateTime.UtcNow.Date - lmp).TotalDays;
+            if (totalDays < 0) return 1;
+
+            var week = (int)(totalDays / 7) + 1;
+            return Math.Clamp(week, 1, 40);
+        }
+    }
+}
---
+++ SafeMum.Application/Services/TranslationService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using SafeMum.Application.Interfaces;
+
+namespace SafeMum.Infrastructure.Services
+{
+    public class TranslationService : ITranslationService
+    {
+        private readonly HttpClient _httpClient;
+        public TranslationService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+            
+        }
+        public async Task<string> TranslateToUrduAsync(string text)
+        {
+            var url = $"https://api.mymemory.translated.net/get?q={Uri.EscapeDataString(text)}&langpair=en|ur";
+
+            var response = await _httpClient.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+
+            var json = await response.Content.ReadAsStringAsync();
+
+            var doc = JsonDocument.Parse(json);
+            return doc.RootElement
+                      .GetProperty("responseData")
+                      .GetProperty("translatedText")
+                      .GetString() ?? string.Empty;
+        }
+    }
+}
