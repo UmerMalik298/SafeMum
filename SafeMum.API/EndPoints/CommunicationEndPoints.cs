87e41c6d3b57206175d87f1a3b0a4d18bb3d30ef
UmerFarooq <umer23672@gmail.com>
2025-07-02 09:15:16 +0500

    user to user communication api

--- SafeMum.API/EndPoints/CommunicationEndPoints.cs
+++ SafeMum.API/EndPoints/CommunicationEndPoints.cs
@@ -1,5 +1,6 @@
 ﻿using MediatR;
 using SafeMum.Application.Features.Communication.GetAllMessages;
+using SafeMum.Application.Features.Communication.GetMessagesByUser;
 using SafeMum.Application.Features.PregnancyTracker.CreateWeeklyProfile;
 using SafeMum.Application.Features.PregnancyTracker.GetWeeklyProfile;
 
@@ -15,6 +16,12 @@ namespace SafeMum.API.EndPoints
            
 
             group.MapGet("/get-all-users-messages", async ([AsParameters] GetAllMessagesRequest request, IMediator mediator) =>
+            {
+                var result = await mediator.Send(request);
+                return Results.Ok(result);
+            });
+
+            group.MapGet("/get-message-by-user-request", async ([AsParameters] GetMessagesByUserRequest request, IMediator mediator) =>
             {
                 var result = await mediator.Send(request);
                 return Results.Ok(result);
---
+++ SafeMum.Application/Features/Communication/GetMessagesUser/GetMessagesUserRequest.cs
@@ -0,0 +1,37 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+using SafeMum.Application.Common.Pagination;
+
+    namespace SafeMum.Application.Features.Communication.GetMessagesByUser
+    {
+        public class GetMessagesByUserRequest : PaginationRequest, IRequest<PaginatedResponse<MessageDto>>
+        {
+            public Guid SenderId { get; set; }
+            public Guid ReceiverId { get; set; }
+
+            public GetMessagesByUserRequest(Guid senderId, Guid receiverId, int pageNumber = 1, int pageSize = 10)
+            {
+                SenderId = senderId;
+                ReceiverId = receiverId;
+                PageNumber = pageNumber;
+                PageSize = pageSize;
+            }
+        }
+    }
+
+    public class MessageDto
+    {
+        public Guid Id { get; set; }
+        public Guid SenderId { get; set; }
+        public Guid ReceiverId { get; set; }
+        public string Content { get; set; }
+        public DateTime SendAt { get; set; }
+        public bool? IsSent { get; set; }
+        public bool? IsSeen { get; set; }
+    }
+
