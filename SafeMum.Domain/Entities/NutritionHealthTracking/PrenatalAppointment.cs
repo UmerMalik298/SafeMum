b40d2db9dea80d1c0d58bfa1232fa9afe07b2b6f
UmerFarooq <umer23672@gmail.com>
2025-07-09 21:38:26 +0500

    nutrition

---
+++ SafeMum.Domain/Entities/NutritionHealthTracking/PrenatalAppointment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Supabase.Postgrest.Models;
+
+namespace SafeMum.Domain.Entities.NutritionHealthTracking
+{
+    public class PrenatalAppointment : BaseModel
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public string DoctorName { get; set; }
+        public string HospitalNamae { get; set; }
+        public DateTime AppointmentDate { get; set; }
+        public string Location { get; set; }
+    }
+}
---
+++ SafeMum.Domain/Entities/NutritionHealthTracking/SupplementLog.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Supabase.Postgrest.Models;
+
+namespace SafeMum.Domain.Entities.NutritionHealthTracking
+{
+    public class SupplementLog : BaseModel
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }  
+        public string Name { get; set; }
+        public string Dosage { get; set; }
+        public DateTime TakenAt { get; set; }
+    }
+}
